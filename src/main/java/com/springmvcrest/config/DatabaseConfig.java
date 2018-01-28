c137ee70cd0deef46730c7770f2a02155c0e5c3c
Маша <gaydukov_s@bigmir.net>
2018-01-28 21:00:41 +0200

    init

--- src/main/java/com/springmvcrest/config/DatabaseConfig.java
+++ src/main/java/com/springmvcrest/config/DatabaseConfig.java
@@ -10,7 +10,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
